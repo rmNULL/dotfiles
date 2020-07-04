ea4158d9b4029f98af22b07178b645dff1c02261
rmnull <rmnull@users.noreply.github.com>
2020-07-05 00:50:27 +0530

    convert link to actual file.

--- bin/pelf-mucking/sub-HB-placeholders.sh
+++ bin/pelf-mucking/sub-HB-placeholders.sh
@@ -1,1 +1,52 @@
-/home/rmnull/dump/builds/elfs/.bin/h.sh
+#!/usr/bin/env bash
+set -e
+
+plain_patchelf=""
+if [[ "$1" = "-p" ]]
+then
+  shift
+  plain_patchelf="1"
+fi
+
+elf="$1"
+[[ -z "$1" ]] && exit 21
+prefix=$(brew --prefix)
+
+chmod +w "$elf"
+if patchelf --print-interpreter "$elf" >/dev/null 2>&1
+then
+  interp=$(patchelf --print-interpreter "$elf")
+  if [[ $interp =~ ^@@HOMEBREW_PREFIX@@.*$ ]]
+  then
+    interp=${interp/@@HOMEBREW_PREFIX@@/"$prefix"}
+
+    if [[ -n $plain_patchelf ]]
+    then patchelf="patchelf"
+    else patchelf="patchelf.rb" 
+    fi
+
+    if ! "$patchelf" --set-interpreter "$interp" "$elf" 
+    then
+      echo "$elf" >> /tmp/fails
+      exit 1
+    fi
+  fi
+fi
+
+
+if patchelf --force-rpath --print-rpath "$elf" >/dev/null 2>&1
+then
+  esc_prefix=$(echo "$prefix" | sed -e 's/\//\\\//g')
+  rpath=$( patchelf --force-rpath --print-rpath "$elf" | tr : '\n' | sed -e "s/@@HOMEBREW_PREFIX@@/${esc_prefix}/" | tr '\n' :)
+
+  if [[ -n $plain_patchelf ]]
+  then patchelf="patchelf"; rp_flag="--force-rpath --set-rpath"
+  else patchelf="patchelf.rb"; rp_flag="--force-rpath --set-runpath"
+  fi
+
+  if ! $patchelf $rp_flag  "${rpath%:}" "$elf"
+  then
+    echo "$elf" >> /tmp/fails
+    exit 2
+  fi
+fi
--- bin/segfault-detection.sh
+++ bin/segfault-detection.sh
@@ -1,1 +1,33 @@
-/home/rmnull/dump/builds/elfs/.bin/self.sh
+#!/usr/bin/env bash
+#
+# Usage: ./segfault-detection.sh <path-to-exec>
+#
+# The script was put together for batch processing,
+# better off not used.
+#
+prg="$1"
+seg_logf="/tmp/segfault.log"
+
+logf=$(echo "$prg" | sed -e 's/\// ; /g')
+logf="/tmp/log/$logf"
+
+"$prg" >"${logf}.out" 2>"${logf}.err" &
+pid="$!"
+
+# segmentation is supposed to happen quick, maybe there's no need to sleep
+sleep 0.2
+
+if kill -0 "$pid" >/dev/null 2>/dev/null
+then
+  exit 0
+else
+  # output already logged above
+  "$prg" >/dev/null 2>/dev/null
+  ec="$?"
+
+  echo -e "$ec $prg" >/tmp/ec.log
+
+  # 139 = SIGSEGV
+  [[ $ec -eq 139 ]] && echo "$prg" >>"$seg_logf" || exit 0
+fi
+
