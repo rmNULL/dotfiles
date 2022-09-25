c0f6081f693a0e07c101fcf0a3141783bdc0e64d
rmnull <rmnull@users.noreply.github.com>
2022-09-25 23:29:00 +0530

    dirty notes backup script

---
+++ bin/notes-backup.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+set -eu
+
+backup_dir="${HOME}/Sync/Backup"
+recipient="rmnull@eos"
+
+if ! [[ -d "$backup_dir" ]]
+then
+    \mkdir -p "$backup_dir"
+fi
+notes_dir="${HOME}/notes"
+readable_date=$(date '+%d-%b-%Y')
+tarball_name="notes-${readable_date}.tar.gz"
+notes_tar="${backup_dir}/${tarball_name}"
+encrypted_notes_tar="${notes_tar}.gpg"
+
+remove_unecrypted_file() {
+    [[ -e "$notes_tar" ]] && rm "$notes_tar"
+}
+
+trap remove_unecrypted_file INT ABRT QUIT
+
+(
+    cd "$(dirname "${notes_dir}")"
+    tar -zcf "${notes_tar}" "$(basename "${notes_dir}")"
+    [[ -e "$encrypted_notes_tar" ]] && rm "${encrypted_notes_tar}"
+    gpg --recipient "$recipient" --output "${encrypted_notes_tar}" --encrypt "${notes_tar}"
+)
+remove_unecrypted_file
